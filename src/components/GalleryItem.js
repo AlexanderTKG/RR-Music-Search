ddef41561902d62259cb93787a5818ac90390588
AlexanderTKG <Alexander-MD@outlook.com>
2022-10-03 02:54:20 -0400

    code updated

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -25,7 +25,14 @@ function GalleryItem(props){
 
     const simpleView = () => {
         return (
-            <div style={simpleStyle}>
+            <div style={{
+                'width': '25vw',
+                'height': '20vw',
+                'border': '1px solid black',
+                'margin': '2px',
+                'position': 'relative',
+
+            }}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
@@ -34,18 +41,19 @@ function GalleryItem(props){
 
     const detailView = () => {
         return (
-            <div style={detailStyle}>
+            <div style={{
+                'width': '80vw',
+                'height': '20vh',
+                'border': '1px solid black',
+                'margin': '2px',
+                'position': 'relative',
+                'backgroundImage': 'url(${props.item.artworkUrl100',
+                'backgroundRepeat': 'no-repeat',
+                'backgroundSize': 'cover',
+                'color': 'white',
+            }}>
                 <h2>{props.item.trackName}</h2>
-                <h3>
-                    <Link to={`/artist/${props.item.artistId}`}>
-                        {props.item.artistName}
-                    </Link>
-                </h3>
-                <h3>
-                    <Link to={`/album/${props.item.collectionId}`}>
-                        {props.item.collectionName}
-                    </Link>
-                </h3>
+                <h3>{props.item.collectionName}</h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
             </div>
@@ -54,8 +62,8 @@ function GalleryItem(props){
     
 
     return (
-        <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
-            <p>One Gallery Item</p>
+        <div onClick={() =>setView(!view)} 
+        style={{'display': 'inline-block'}}>
                 {view ? detailView() : simpleView()}
         </div>
     )
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -6,8 +6,7 @@ function SeachBar(props) {
 
     return (
         <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-
-            <input type="text" placeholder="Enter a search term here" onChange={
+            <input type="text" placeholder="Search Here" onChange={
                 (e) => setSearchTerm(e.target.value)
             }/>
 
